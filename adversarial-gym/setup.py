def5b6ec7416162458811253dc9ca89f2687947f
Yash Sahijwani <60649571+Terabyte17@users.noreply.github.com>
2021-01-04 15:48:54 +0530

    versions of dependencies fixed

--- adversarial-gym/setup.py
+++ adversarial-gym/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='adversarial_gym',
       version='0.0.1',
-      install_requires=['gym', 'tensorflow', 'pybullet'] 
+      install_requires=['gym==0.18.0', 'pybullet==2.8.3'] 
 )
